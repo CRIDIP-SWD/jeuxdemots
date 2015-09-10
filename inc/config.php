3aa9a97bd38850c326fd16593bf8e1fe298212e7
CRIDIP-SWD <mmockelyn@cridip.com>
2015-09-10 11:28:04 +0200

    JEUX DE MOTS

---
+++ inc/config.php
@@ -0,0 +1,20 @@
+<?php
+//DEFINE SITE
+DEFINE("ROOT", "http://vps162634.ovh.net/jeuxdemots/");
+DEFINE("ASSETS", "assets/");
+DEFINE("CSS", "css/");
+DEFINE("IMG", "img/");
+DEFINE("JS", "js/");
+
+DEFINE("NOM_SITE", "JEUX DE MOTS");
+
+
+function nom_sector($sector)
+{
+    return $sector;
+}
+
+function nom_page($page)
+{
+    return $page;
+}
