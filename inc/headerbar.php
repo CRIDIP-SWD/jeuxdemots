acff29be483e7adeab6f21f33b8088f21c47dbae
CRIDIP-SWD <mmockelyn@cridip.com>
2015-09-10 15:08:41 +0200

    JEUX DE MOTS

---
+++ inc/headerbar.php
@@ -0,0 +1,34 @@
+
+<!-- nav mobile bars -->
+<div class="navigation-toggle">
+    <div class="navigation-toggle-button"><span class="fa fa-bars"></span></div>
+</div>
+<!-- ./nav mobile bars -->
+
+<!-- navigation -->
+<ul class="navigation">
+    <li>
+        <a href="#">ACCUEIL</a>
+    </li>
+    <li>
+        <a href="#">Annonceurs</a>
+        <ul>
+            <li>
+                <a href="about-us.html">Pays de la Loire</a>
+                <ul>
+                    <li><a href="">Vendée</a></li>
+                </ul>
+            </li>
+        </ul>
+    </li>
+    <li>
+        <a href="#">Jeux</a>
+        <ul>
+            <li><a href="blog-grid.html">Mots Fléché</a></li>
+            <li><a href="blog-post.html">Mots Croisée</a></li>
+            <li><a href="">Sudoku</a></li>
+        </ul>
+    </li>
+    <li><a href="../html/index.html">Contact</a></li>
+</ul>
+<!-- ./navigation -->
--- index.php
+++ index.php
@@ -11,13 +11,13 @@
 
             <!-- page logo -->
             <div class="logo">
-                <a href="index.html">Atlant - Front-end Template</a>
+                <a href="<?= ROOT; ?>"></a>
             </div>
             <!-- ./page logo -->
 
 
             <!-- search -->
-            <div class="search">
+            <!--<div class="search">
                 <div class="search-button"><span class="fa fa-search"></span></div>
                 <div class="search-container animated fadeInDown">
                     <form action="#" method="post">
@@ -29,51 +29,9 @@
                         </div>
                     </form>
                 </div>
-            </div>
+            </div>-->
             <!-- ./search -->
-
-            <!-- nav mobile bars -->
-            <div class="navigation-toggle">
-                <div class="navigation-toggle-button"><span class="fa fa-bars"></span></div>
-            </div>
-            <!-- ./nav mobile bars -->
-
-            <!-- navigation -->
-            <ul class="navigation">
-                <li>
-                    <a href="#">Home</a>
-                    <ul>
-                        <li><a href="index.html">With Slider</a></li>
-                        <li><a href="index-default.html">Default</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#">Pages</a>
-                    <ul>
-                        <li><a href="about-us.html">About Us</a></li>
-                        <li><a href="contacts.html">Contacts</a></li>
-                        <li><a href="pricing.html">Pricing</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#">Blog</a>
-                    <ul>
-                        <li><a href="blog-grid.html">Blog Grid</a></li>
-                        <li><a href="blog-post.html">Blog Post</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#">Portfolio</a>
-                    <ul>
-                        <li><a href="portfolio-with-title.html">Portfolio With Title</a></li>
-                        <li><a href="portfolio-2column.html">Portfolio 2 Column</a></li>
-                        <li><a href="portfolio-3column.html">Portfolio 3 Column</a></li>
-                        <li><a href="portfolio-4column.html">Portfolio 4 Column</a></li>
-                    </ul>
-                </li>
-                <li><a href="../html/index.html">Admin Template</a></li>
-            </ul>
-            <!-- ./navigation -->
+            <?php include('inc/headerbar.php'); ?>
 
 
         </div>
